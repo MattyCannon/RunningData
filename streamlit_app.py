f41e1ae6926a125faafadd7c477a802a7357195f
MattyCannon <106330890+MattyCannon@users.noreply.github.com>
2023-04-18 11:05:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ import os
 #df_activities = sc.get_fromAPI(sc.get_auth(), "activities")
 df_activities = pd.read_excel('data1.xlsx')
 
-#summary = df_activities.head
+summary = df_activities.head
 st.header('My Running Data')
 st.header('Recent Runs: ')
-#st.table(summary)
+st.table(summary)
 
 #print(df_activities.columns)
 #longest_run = max(df_activities['distance'])
