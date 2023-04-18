4e5a9e92ee23fd6c2d80db2e12e995a27f93a4c5
MattyCannon <106330890+MattyCannon@users.noreply.github.com>
2023-04-18 10:36:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import stravaConnect
-df_activities = get_auth()
+import stravaConnect as sc
+df_activities = sc.get_fromAPI(sc.get_auth(), "activities")
 
 st.header('Test')
 st.table(df_activities)
