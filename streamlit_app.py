22d6adce094717fe678158e1f721acec7fd89923
MattyCannon <106330890+MattyCannon@users.noreply.github.com>
2023-04-18 10:50:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import stravaConnect as sc
 import streamlit as st
+
 df_activities = sc.get_fromAPI(sc.get_auth(), "activities")
 
-st.header('Test 2')
-st.table(df_activities)
+print(df_activities.head)
+st.header('My Running Data')
+st.header('Recent Runs: ')
+st.table(df_activities.head)
+
+print(df_activities.columns)
+longest_run = max(df_activities['distance'])
+
+st.write(f'longest run: {longest_run}')
